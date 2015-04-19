2bb053e9cddddf3b8bb5c9ba7ef4a66850062aa7
EvilEntity <me@piotrjastrzebski.io>
2015-04-19 02:08:33 +0200

    added new msg id

--- core/src/io/piotrjastrzebski/ld32/game/Msg.java
+++ core/src/io/piotrjastrzebski/ld32/game/Msg.java
@@ -1,8 +1,10 @@
 package io.piotrjastrzebski.ld32.game;
 
 /**
+ * Ids for messages
  * Created by EvilEntity on 18/04/2015.
  */
 public class Msg {
 	public final static int MSG_ = 0;
+	public final static int FIRE_MILK_MISSILE = 1;
 }
